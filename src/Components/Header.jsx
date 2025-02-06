867291ca84bba1ee24a1d3873b711cd5517937c6
Hassam Kiani <143179697+hassamk122@users.noreply.github.com>
2025-02-06 18:17:04 +0500

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <>
       <div className='flex justify-around items-center text-center place-items-center  border-gray-700  dark:border-gray-200  border-b-[1px] pb-4 mx-1 sm:mx-4'>
         <div className='flex items-center justify-center mt-6'>
-          <Link to="/"><img className='w-8 h-8' src={AuraAudit} /></Link>
+          <Link to="/"><img className='w-8 h-8' src={AuraAudit} loading="lazy" /></Link>
           <h2 className='font-Poppins text-2xl font-bold mx-3 text-white  dark:text-dark-purple'><Link to="/">La passion</Link></h2>
         </div>
         <div className='flex items-center flex-row mt-6 '>
